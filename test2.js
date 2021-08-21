645168fb2d3748839da8d5c275c6b8e07d35daf9
zhouwei441300 <zhouwei441300@163.com>
2021-08-21 20:26:14 +0800

    note

--- test2.js
+++ test2.js
@@ -1,1 +1,43 @@
-﻿console.log("hello test2 ......")
+﻿console.log("hello test2 ......");
+
+function test1(a, b) {
+    return a + b;
+}
+
+console.log(test1(1, 2))
+
+// 创建对象 方式一
+var o1 = new Object();
+// 创建对象 方式二 类似map 对象字面量（object literal）
+var o2 = {};
+
+// “对象字面量”也可以用来在对象实例中定义一个对象
+var o3 = {
+    name: "Carrot",
+    _for: "Max",//'for' 是保留字之一，使用'_for'代替
+    details: {
+        color: "orange",
+        size: 12
+    }
+};
+console.log(o3.details.color);
+// map形式访问
+console.log(o3["details"]["size"]);
+
+// 创建了一个对象原型 Person
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+// 定义一个对象
+var You = new Person('You', 24);
+console.log(You.name);
+
+
+
+
+
+
+
+
+
