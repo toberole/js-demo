2d6dca680857c1fa66320fecd0abfea73c9f73b0
zhouwei <toberole@163.com>
2021-10-30 16:24:50 +0800

    note

---
+++ demo1/test3.js
@@ -0,0 +1,9 @@
+import Student from "./Student.js"
+
+function test1(){
+    console.log("Hello World!");
+}
+
+let stu = new Student("hello","11");
+stu.printStu()
+test1();
---
+++ demo1/test4.js
@@ -0,0 +1,15 @@
+class Test4 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    printInfo() {
+        console.log(this.name);
+        console.log(this.age)
+    }
+}
+
+let stu = new Test4("hello", "1");
+console.log(stu.age);
+console.log(stu.name);
